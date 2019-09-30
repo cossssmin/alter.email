fa4285c79bbc0832241fb626932a62d162a7c768
Cosmin Popovici <hellocosmin@gmail.com>
2019-09-30 12:15:54 +0300

    Stop removing attribute quotes in production 🤦‍♂️

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://alter.email/'
-    : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? 'https://alter.email/' : '/',
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(([args]) => {
+        args.xhtml = true
+        args.minify.removeAttributeQuotes = false
+        return [args]
+      })
+  }
 }
